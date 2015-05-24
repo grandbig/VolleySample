1c72549fc2321026f27687c19ca242ef4dddf87e
加藤 雄大 <takahiro.kato15@gmail.com>
2015-05-24 21:53:13 +0900

    volleyの追加と実装

--- app/src/main/java/com/example/takahiro/volleysample/MainActivity.java
+++ app/src/main/java/com/example/takahiro/volleysample/MainActivity.java
@@ -2,16 +2,49 @@ package com.example.takahiro.volleysample;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private RequestQueue mRequestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp";
+
+        mRequestQueue.add(new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // 処理が成功した場合の処理
+                Log.d("VolleySample", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // エラーが発生した場合の処理
+                Log.d("VolleySample", error.toString());
+            }
+        }));
     }
 
     @Override
